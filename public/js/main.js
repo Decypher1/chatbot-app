74bbf12f1e61b3f0826c13834343b0a4d70dd99f
decypher1 <mumekwe2@gmail.com>
2023-03-18 06:49:12 +0100

    Updated server and html file

--- public/js/main.js
+++ public/js/main.js
@@ -1,58 +1,59 @@
 const socket = io();
-const btn = document.querySelector('.btn');
-const chatBody = document.querySelector('.chat-body');
-const msg = document.getElementById('msg');
 
 
-//function to append message to the chatbody
+const msgChat = document.querySelector('.chat-box');
+const msgForm = document.querySelector(".msg-inputarea");
+const msgInput = document.getElementById("msg-input");
 
-function appendMessage(message, sender) {
-    const messageElement = document.createElement('div');
-    messageElement.className = "message-text";
-    messageElement.id =sender;
-    messageElement.textContent = message;
+const person_name = ""
 
-    const timestamp = new Date().toLocaleTimeString(); // timestamp
-    const timestampElement = document.createElement('span');
-    timestampElement.className = "timestamp";
-    timestampElement.textContent = timestamp;
-
-    const messageContainer = document.createElement('div');
-    const messageContainer2 = document.createElement('div');
-    messageContainer.className = "message-container " + sender;
-    messageContainer2.className = "message-container2 " + sender;
-    messageElement.innerHTML = message.replace(/\n/g, '<br>');
-    messageContainer2.appendChild(messageContainer);
-    messageContainer.appendChild(messageElement);
-    messageContainer.appendChild(timestampElement);
-    chatBody.appendChild(messageContainer2);
-    chatBody.scrollTop = chatBody.scrollHeight;
-}
-
-
-//Handling sending messages
-function sendMessage() {
-    const message = msg.value;
-    if (message) {
-        return;
-    }
-    appendMessage(message, "user");
-    socket.emit('message', message);
-    msg.value = '';
-}   
-
-
-//Message from server
-socket.on('message', message => {
-    console.log(message);
-    appendMessage(message);
+socket.on("auto-response", (message) => {
+    botResponse(message)
 })
 
 
-//Event Listener
-btn.addEventListener('click', sendMessage);
-msg.addEventListener('keydown', (e) => {
-    if (e.key === "Enter") {
-        sendMessage();
+msgForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let msgText = msgInput.value;
+    msgText = msgText.trim();
+    if (!msgText) {
+        return false;
     }
+    socket.emit("chatMessage", msgText)
+    appendMessage(person_name, "right", msgText);
+    msgInput.value = "";
+
 });
+
+function appendMessage(name, side, text) {
+    const msgHTML = `
+    <div class="msg ${side}-msg">
+        <div class="msg-bubble">
+            <div class="msg-info">
+                <div class="msg-info-name">${name}</div>
+                <div class="msg-info-time">${formatDate(new Date())}</div>  
+            </div>
+
+            <div class="msg-text">${text}</div>
+        </div>
+    </div>
+    `;
+
+    msgChat.insertAdjacentHTML("beforeend", msgHTML);
+    msgChat.scrollTop = msgChat.scrollHeight;
+}
+
+function botResponse(message) {
+    setTimeout(() => {
+        appendMessage("Cypher", "left", message);
+    }, 1000);
+}
+
+function formatDate(date) {
+    const h = "0" + date.getHours();
+    const m = "0" + date.getMinutes();
+
+    return `${h.slice(-2)}:${m.slice(-2)}`;
+}
+
--- server.js
+++ server.js
@@ -1,238 +1,249 @@
-const PATH = require('path')
-const express = require('express');
+const express = require("express");
+const app = express();
 const http = require("http");
 const CONFIG = require('./config/config')
-const socketio = require('socket.io');
-
-const app = express();
+const { Server } = require("socket.io");
 const server = http.createServer(app);
-const io = socketio(server)
+const io = new Server(server);
 const session = require("express-session");
+const path = require("path");
 
 
-// Setting static folder
-app.use(express.static(PATH.join(__dirname, 'public')));
-
-const menu = {
-	11: "Rice & Chicken",
-	12: "HamBurger",
-	13: "Meatpie",
-	14: "Sharwarma",
-	15: "Eba and Egusi"
-} 
 
 
-// Use session middleware
 const sessionMW = session({
-  secret: CONFIG.sessionSecret,
+  secret: "Decypher",
   resave: false,
   saveUninitialized: true,
-  cookie: {
-    secure: false,
-    maxAge: 1000 * 60 * 60 * 24 * 7,
-  },
 });
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(sessionMW);
 
 
-const orderHistory = [];
 
 io.use((socket, next) => {
-	sessionMW(socket.request, socket.request.res, next);
+  sessionMW(socket.request, socket.request.res, next);
 });
-
 io.on("connection", (socket) => {
-	console.log("User connected:", socket.id);
+  const orderHistory = [];
+  console.log("New customer in", socket.id);
+  let customer = {
+    name: "",
+    cart: [],
+  };
+  const check = async () => {
+    if (customer.cart === null) {
+      await autoResponse(`Input <b>1</b> to view list of available menu.`);
+    }
+  };
 
-	// Get unique identifier for user's device
-	const deviceId = socket.handshake.headers["user-agent"];
+  const autoResponse = async (msg) => {
+    socket.emit("auto-response", msg);
+  };
 
-	// Check if user already has an existing session
-	if (
-		socket.request.session[deviceId] &&
-		socket.request.session[deviceId].userName
-	) {
-		// If user already has a session, use existing username and current order
-		socket.emit(
-			"message",
-			`Welcome back, ${
-				socket.request.session[deviceId].userName
-			}! You have a current order of ${socket.request.session[
-				deviceId
-			].currentOrder.join(", ")}`
-		);
-	} else {
-		// If user does not have a session, create a new session for user's device
-		socket.request.session[deviceId] = {
-			userName: "",
-			currentOrder: [],
-			deviceId: deviceId, // store deviceId in session object
-		};
-	}
+  const userResponse = async (msg) => {
+    try {
+      const menu = [
+        { name: "RIce & chicken", price: 3500, tag: "3", qty: 1},
+        { name: "Hamburger", price: 2000, tag: "4", qty: 1 },
+        { name: "Meatpie", price: 1000, tag: "5", qty: 1 },
+        { name: "Sharwarma", price: 1500, tag: "6", qty:1 },
+      ];
+      if (!customer.name) {
+        customer.name = msg;
+        await autoResponse(
+          `welcome to Cyphers food, <b>${customer.name}!</b> <br>Press <b>1</b> to view list of available menu.`
+        );
+      } else {
+        switch (msg) {
+          case "1":
+            // Generate the list of items dynamically
+            const itemOptions = menu
+              .map((food) => `<b>${food.name}</b> => <b>${food.tag}</b><br>`)
+              .join("\n");
+            // customer.cart = [];
+            await autoResponse(
+              `Available products and their respective tags:<br>${itemOptions}Please select a product by inputing its corresponding tag.`
+            );
+            break;
+          case "3":
+            check();
+            let Item1 = menu.find((val) => val.tag === "3");
+            const item1Index = customer.cart.findIndex(
+              (val) => val.tag === "3"
+            );
+            if (item1Index === -1) {
+              customer.cart.push(Item1);
+            } else {
+              customer.cart[item1Index].qty++
+            }
+            await autoResponse(
+              `<b><i>${Item1.name}</i></b> has been added to your order list. <br>Input <b>1</b> to view menu and place more orders. <br>Input <b>97</b> to view order list.<br>Input <b>99</b> to checkout order list.<br>Input <b>0</b> to clear order list.`
+            );
+            break;
+          case "4":
+            check();
+            let Item7 = menu.find((val) => val.tag === "4");
+            const item7Index = customer.cart.findIndex(
+              (val) => val.tag === "4"
+            );
+            if (item7Index === -1) {
+              customer.cart.push(Item7);
+            } else {
+              customer.cart[item7Index].qty++
+            }
+            await autoResponse(
+              `<b><i>${Item7.name}</i></b> has been added to your order list. <br>Input <b>1</b> to view menu and place more orders. <br>Input <b>97</b> to view order list.<br>Input <b>99</b> to checkout order list.<br>Input <b>0</b> to clear order list.`
+            );
+            break;
+          case "5":
+            check();
+            const Item2 = menu.find((val) => val.tag === "5");
+            const item2Index = customer.cart.findIndex(
+              (val) => val.tag === "5"
+            );
+            if (item2Index === -1) {
+              customer.cart.push(Item2);
+            } else {
+              customer.cart[item2Index].qty++
+            }
+            await autoResponse(
+              `<b><i>${Item2.name}</i></b> has been added to your order list. <br>Input <b>1</b> to view menu and place more orders. <br>Input <b>97</b> to view order list.<br>Input <b>99</b> to checkout order list.<br>Input <b>0</b> to clear order list.`
+            );
+            break;
+          case "6":
+            check();
+            const Item4 = menu.find((val) => val.tag === "6");
+            const item4Index = customer.cart.findIndex(
+              (val) => val.tag === "6"
+            );
+            if (item4Index === -1) {
+              customer.cart.push(Item4);
+            } else {
+              customer.cart[item4Index].qty++
+            }
+            await autoResponse(
+              `<b><i>${Item4.name}</i></b> has been added to your order list. <br>Input <b>1</b> to view menu and place more orders. <br>Input <b>97</b> to view order list.<br>Input <b>99</b> to checkout order list.<br>Input <b>0</b> to clear order list.`
+            );
+            break;
+          case "99":
+            check()
+            if (customer.cart.length === 0) {
+              await autoResponse(
+                "No orders to checkout. Please input <b>1</b> to place order."
+              );
+              return
+            } 
+            if(orderHistory.length === 0) {
+              customer.cart.map((prod) => orderHistory.push(prod));
+              customer.cart = [];
+              await autoResponse(
+                "Order checked out successfully! <br>Input <b>1</b> to view menu and place more orders.<br>Input <b>98</b> to view checked out order(s)."
+              );
+              return
+            }
+            else{
+              const bar = JSON.parse(JSON.stringify(customer.cart))
+              for (let i = 0; i < bar.length; i++) {
+                const key1 = bar[i].name;
+                for (let j = 0; j < orderHistory.length; j++) {
+                  const key2 = orderHistory[j].name;
+                  if (key1 === key2) {
+                    orderHistory[j].qty +=bar[i].qty
+                    const rem = bar.findIndex((val)=> val.name===key1)
+                    delete customer.cart[rem]
+                    break
+                  }
+                }
+              }
+              customer.cart.map((prod) => orderHistory.push(prod));
+              customer.cart = [];
+              await autoResponse(
+                "Order checked out successfully! <br>Input <b>1</b> to view menu and place more orders.<br>Input <b>98</b> to view checked out order(s)."
+              );
+            }
+            break;
+          case "98":
+            check()
+            if (orderHistory.length === 0) {
+              await autoResponse(
+                "No previous checked out orders. Please input <b>1</b> to view menu and place order."
+              );
+            } 
+            else {
+              const price = orderHistory.reduce((acc, val) => {
+                return acc + (val.price * val.qty);
+              }, 0);
 
-	// Ask for user's name if not provided already	
-	if (!socket.request.session[deviceId].userName) {
-		socket.emit("message", "Hello! Please enter your name");
-		
-	} else {
-		socket.emit(
-			"message",
-			`Welcome back, ${
-				socket.request.session[deviceId].userName
-				}! You have a current order of ${socket.request.session[
-					deviceId
-					].currentOrder.join(", ")}`
-		);
-	}
+              const orderHistoryString = orderHistory
+                .map(
+                  (order) =>
+                    `<b>${order.name} Qty:${order.qty} Price:${order.price} </b> each<br>`
+                )
+                .join("\n");
+              await autoResponse(
+                `Here are your previous checked out orders:<br>${orderHistoryString} <b>Total cost:${price}</b><br>Input <b>1</b> to view menu and place more orders.`
+              );
+            }
+            break;
+          case "97":
+            check()
+            if (customer.cart.length === 0) {
+              await autoResponse(
+                "No current order.<br>Input <b>1</b> to view menu and place more orders.<br>Input <b>99</b> to checkout order list."
+              );
+            }  
+            else {
+              const price = customer.cart.reduce((acc, val) => {
+                return acc + val.price * val.qty;
+              }, 0);
+              const currentOrder = customer.cart
+                .map(
+                  (order) =>
+                    `<b>${order.name} Qty:${order.qty} Price:${order.price}</b> each<br>`
+                )
+                .join("\n");
 
-	let userName = socket.request.session[deviceId].userName;
+              await autoResponse(
+                `Here is your current order:<br>${currentOrder} <b>Total cost:${price}</b><br>Input <b>0</b> to cancel orders.<br>Input <b>1</b> to view menu and place more orders.`
+              );
+            }
+            break;
+          case "0":
+            check()
+            if (customer.cart.length === 0) {
+              await autoResponse(
+                "You have no placed order to cancel. Please input <b>1</b> to start placing orders."
+              );
+            } 
+            else {
+              customer.cart = [];
+              await autoResponse(
+                "Order cancelled. Input <b>1</b> to view menu and start placing orders."
+              );
+            }
+            break;
+          default:
+            await autoResponse(
+              "Invalid input <br>Please input <b>1</b> to view menu."
+            );
+        }
+      }
+    } catch (err) {
+      console.log(err.message);
+    }
+    // socket.emit("user-response", msg)
+  };
 
-	//Listen for bot message
-	socket.on("message", (message) => {
-		console.log('message received:', message);
-		socket.emit("message", message);
-	});
-
-	//Listening for incoming messages
-	socket.on("user-message", (message) => {
-		console.log('User message received:', message);
-
-		if (!userName) {
-			userName = message;
-			socket.request.session[deviceId].userName = userName;
-			socket.emit(
-				"message",
-				`Welcome to Cypher food ChatBot, ${userName}!\n1. Place an order\n99. Checkout order\n98. Order history\n97. Current order\n0. Cancel order`
-			);
-		} else {
-			switch (message) {
-				case "1":
-					const menuList = Object.keys(menu)
-						.map((item) => `${item}. ${menu[item]}`)
-						.join("\n");
-					socket.emit(
-						"message",
-						`Please select an item from the menu using the number:\n${menuList}`
-					);
-					break;
-					// Showing current order
-				case "97":
-					if (socket.request.session[deviceId].currentOrder.length > 0) {
-						const currentOrder = socket.request.session[deviceId].currentOrder.join(', ');
-						socket.emit(
-							"message",
-							`Your current order is: ${currentOrder}\n1. Place an order\n99. Checkout order\n98. Order history\n97. Current order\n0. Cancel order`
-						);
-					} else {
-						socket.emit(
-							"message",
-							`You have no current order.\n1. Place an order\n99. Checkout order\n98. Order history\n97. Current order\n0. Cancel order`
-						);
-					}
-					break;
-
-				//checkout the order
-				case "99":
-					if (socket.request.session[deviceId].currentOrder.length > 0) {
-						const currentOrder = socket.request.session[deviceId].currentOrder.join(', ');
-						socket.emit(
-							"message",
-							`Your order of ${currentOrder} has been placed. Thank you for your patronage.`
-						);
-						orderHistory.push({
-							deviceId: socket.request.session[deviceId].deviceId,
-							userName: socket.request.session[deviceId].userName,
-							order: socket.request.session[deviceId].currentOrder,
-							date: new Date().toLocaleString(),
-						});
-						socket.request.session[deviceId].currentOrder = [];
-					} else {
-						socket.emit(
-							"message",
-							`You have no current order.\n1. Place an order\n99. Checkout order\n98. Order history\n97. Current order\n0. Cancel order`
-						);
-					}
-					break;
-					//showing order history
-				case "98":
-					if (orderHistory.length > 0) {
-						const orderHistoryList = orderHistory
-							.map((item) => `Order: ${item.order.join(', ')}\nDate: ${item.date}\n`)
-							.join("\n");
-						socket.emit(
-							"message",
-							`Your order history is:\n${orderHistoryList}\n1. Place an order\n99. Checkout order\n98. Order history\n97. Current order\n0. Cancel order`
-						);
-					} else {
-						socket.emit(
-							"message",
-							`You have no order history.\n1. Place an order\n99. Checkout order\n98. Order history\n97. Current order\n0. Cancel order`
-						);
-					}
-					break;
-					//cancel order
-				case "0":
-					const currentOrder = socket.request.session[deviceId].currentOrder.join(', ');
-					if (currentOrder.length === 0 && orderHistory.length === 0) {
-						socket.emit(
-							"message",
-							`You have no current order.\n1. Place an order\n99. Checkout order\n98. Order history\n97. Current order\n0. Cancel order`
-						);
-					} else {
-						socket.request.session[deviceId].currentOrder = [];
-						orderHistory.length = 0;
-
-						socket.emit(
-							"message",
-							`Your order of ${currentOrder.join(', ')} has been cancelled.`
-						);
-					}
-					break;
-				default:
-					const itemNumber = parseInt(message);
-					if (!isNaN(itemNumber) && menu[itemNumber]) {
-						socket.request.session[deviceId].currentOrder.push(
-							menu[itemNumber]
-						);
-						socket.emit(
-							'message',
-							`You have added ${menu[itemNumber]} to your order.\n1. Place an order \n99. Checkout order\n98. Order history\n97. Current order \n0. Cancel order`
-						);
-					} else {
-						socket.emit(
-							"message",
-							`Invalid option. Please select a valid option.\n1. Place an order\n99. Checkout order\n98. Order history\n97. Current order\n0. Cancel order`
-						);
-					}
-					break;
-			}
-		}
-	});
-	socket.on('disconnect', () => {
-		delete socket.request.session[deviceId];
-	
-		console.log('User disconnected', socket.id);
-	});
+  socket.on("chatMessage", (msg) => {
+    userResponse(msg);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
 server.listen(CONFIG.PORT, () => {
-	console.log(`Server running on port ${CONFIG.PORT}`);
+  console.log(`server running on port: ${CONFIG.PORT}`);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
